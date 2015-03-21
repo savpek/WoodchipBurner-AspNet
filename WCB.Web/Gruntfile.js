cfd06fa0acca7d9370dde25bab0d8369b9af9e26
Savpek <savolainen.pekka@gmail.com>
2015-03-21 12:53:50 +0200

    Added working bower + concat.

---
+++ WCB.Web/Gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "bower_components",
+                    layout: "byComponent",
+                    cleanTargetDir: true
+                }
+            }
+        },
+        concat: {
+            options: {
+            },
+            bower: {
+                files: {
+                    "wwwroot/bower_concat.js": ["bower_components/jquery/dist/*.js", "bower_components/**/dist/**/*.js", "!bower_components/**/*.min.js"],
+                    "wwwroot/bower_concat.css": ["bower_components/**/*.css", "!bower_components/**/*.min.css"]
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["bower:install", "concat:bower"]);
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+};
